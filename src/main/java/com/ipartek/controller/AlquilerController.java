5c0b697ff16984f59607c0ad85f4879587b0a03f
Alain <107689010+ipartekAlain@users.noreply.github.com>
2024-02-06 12:28:24 +0100

    ALA_CONTROLADORES_ALQUILER
    Creacion de los controladores de alquiler

---
+++ src/main/java/com/ipartek/controller/AlquilerController.java
@@ -0,0 +1,11 @@
+package com.ipartek.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class AlquilerController {
+
+	 @Autowired
+	 private IViviendaService viviendaServ;
+	
+}
